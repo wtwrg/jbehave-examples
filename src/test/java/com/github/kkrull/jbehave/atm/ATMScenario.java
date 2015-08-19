efc4c3d496948def538b5da8ef0ffeccbbb1efcf
Kyle Krull <kyle.krull@here.com>
2015-08-19 16:27:33 -0500

    Runnable in IDE as JUnit test

--- src/test/java/com/github/kkrull/jbehave/atm/ATMScenario.java
+++ src/test/java/com/github/kkrull/jbehave/atm/ATMScenario.java
@@ -4,34 +4,30 @@ import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.junit.JUnitStory;
-import org.jbehave.core.reporters.*;
+import org.jbehave.core.reporters.ConsoleOutput;
+import org.jbehave.core.reporters.SilentSuccessFilter;
 import org.jbehave.core.steps.CandidateSteps;
 import org.jbehave.core.steps.InstanceStepsFactory;
+import org.junit.Test;
 
 import java.util.List;
 
 public class ATMScenario extends JUnitStory {
 
-    // Here we specify the configuration, starting from default MostUsefulConfiguration, and changing only what is needed
+    @Test //Make this recognizable to IDEs so you can run the scenario directly without making an extra Runner class.
+    public void run() throws Throwable { super.run(); }
+
     @Override
     public Configuration configuration() {
-        StoryReporterBuilder reporterBuilder = new StoryReporterBuilder()
-            .withDefaultFormats()
-            .withFormats(Format.CONSOLE, Format.TXT, Format.HTML, Format.XML);
-        StoryReporter filter = new SilentSuccessFilter(new ConsoleOutput());
         Configuration configuration = new MostUsefulConfiguration()
-            .useDefaultStoryReporter(filter)
             .useStoryLoader(new LoadFromClasspath(this.getClass()))
-//            .useStoryReporterBuilder(reporterBuilder)
-            ;
+            .useDefaultStoryReporter(new SilentSuccessFilter(new ConsoleOutput()));
 
         return configuration;
     }
 
-    // Here we specify the steps classes
     @Override
     public List<CandidateSteps> candidateSteps() {
-        // varargs, can have more that one steps classes
         return new InstanceStepsFactory(configuration(), new ATMScenarioSteps())
             .createCandidateSteps();
     }
