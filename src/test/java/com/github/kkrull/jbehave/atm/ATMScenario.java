652f9336adf3c08ea3bbf2335325c6b6998e25fe
Kyle Krull <kyle.krull@here.com>
2015-08-21 14:49:23 -0500

    No clear way to limit test output any further at this point.

--- src/test/java/com/github/kkrull/jbehave/atm/ATMScenario.java
+++ src/test/java/com/github/kkrull/jbehave/atm/ATMScenario.java
@@ -4,39 +4,29 @@ import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.junit.JUnitStory;
-import org.jbehave.core.reporters.ConsoleOutput;
-import org.jbehave.core.reporters.Format;
-import org.jbehave.core.reporters.SilentSuccessFilter;
-import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.reporters.*;
 import org.jbehave.core.steps.CandidateSteps;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.jbehave.core.steps.SilentStepMonitor;
+import org.jbehave.core.steps.StepCollector;
 import org.junit.Test;
 
 import java.util.List;
 
 import static org.jbehave.core.reporters.Format.*;
 
-public class ATMScenario extends JUnitStory {
+public class ATMScenario extends JUnitStory { //TODO KDK: Single class for all stories?
     @Test //Make this recognizable to IDEs so you can run the scenario directly without making an extra Runner class.
     public void run() throws Throwable { super.run(); }
 
-    //TODO KDK: Where is the [INFO] output coming from when running `mvn integration-test`?  It all goes to stdout.
     //TODO KDK: Find a more robust mapping of JUnitStory -> .story, or at least one that is better at identifying failures
     @Override
     public Configuration configuration() {
-        StoryReporterBuilder storyReporter = new StoryReporterBuilder()
-            .withDefaultFormats()
-            .withFormats(ANSI_CONSOLE, HTML)
-            ;
-        Configuration configuration = new MostUsefulConfiguration()
+        StoryReporterBuilder storyReporter = new StoryReporterBuilder().withFormats(ANSI_CONSOLE, HTML);
+        return new MostUsefulConfiguration()
             .useStoryLoader(new LoadFromClasspath(this.getClass()))
             .useStoryReporterBuilder(storyReporter)
-            .useStepMonitor(new SilentStepMonitor())
-//            .useDefaultStoryReporter(new SilentSuccessFilter(new ConsoleOutput()))
-            ;
-
-        return configuration;
+            .useStepMonitor(new SilentStepMonitor());
     }
 
     @Override
